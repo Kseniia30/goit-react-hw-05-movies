26374538e65a15634504d93c0cc9d8d60c91b14f
Kseniia30 <kellyshikova3004@gmail.com>
2022-10-26 18:57:01 +0200

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,26 @@
 import { fetchTrending } from "fetch/fetchTrending"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useLocation } from "react-router-dom"
 import { MainItem, MainLink, MainList, HomeTitle } from "./Home.styled"
 
 const HomeMain = () => {
     const [movies, setMovies] = useState([])
     const location = useLocation()
+    const isFirstRender = useRef(true)
 
     useEffect(() => {
-        console.log("render", Date.now());
-        fetchTrending().then(res => {
-        const results = res.data.results
-            setMovies(results)
-    })
+        if (isFirstRender.current) {
+            fetchTrending().then(res => {
+            const results = res.data.results
+                setMovies(results)
+        })
+            isFirstRender.current = false
+        }
     }, [])
 
     if (!movies) {
         return null
     }
-
 
     return (
         <main>
--- src/fetch/fetchTrending.js
+++ src/fetch/fetchTrending.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 const key="7cdf478795ad4657f8647bebd5408f4d"
 
-export const fetchTrending = (page) => {
+export const fetchTrending = () => {
     return axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${key}`)
 }
